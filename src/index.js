03614a88be2d29175559c747f723022d1d21b540
Lucas Kalado <kalado@gmail.com>
2019-05-09 14:56:20 -0300

    Adiciona Component Icon em index.js para utilizar na plataforma

--- src/index.js
+++ src/index.js
@@ -45,6 +45,7 @@ export { default as Switch } from './components/Switch';
 export { default as Appbar } from './components/Appbar/Appbar';
 export { default as TouchableRipple } from './components/TouchableRipple';
 export { default as TextInput } from './components/TextInput/TextInput';
+export { default as Icon } from './components/Icon';
 export {
   default as ToggleButton,
 } from './components/ToggleButton/ToggleButton';
